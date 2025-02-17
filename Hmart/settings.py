d5651c42008a8597c703f22925def5c5602fbc64
cincy-coder <junocincy@gmail.com>
2025-02-17 13:33:28 +0530

    settings.py

--- Hmart/settings.py
+++ Hmart/settings.py
@@ -95,24 +95,24 @@ WSGI_APPLICATION = 'Hmart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
-'''
+}'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hmart_db',
-        'USER': 'root',
-        'PASSWORD': 'tymfugit',
+        'NAME': 'hmart_ecom_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '3306',
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
