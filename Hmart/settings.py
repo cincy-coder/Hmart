4740f55cc7e1b4f878e7db852e388d3bc7c4f367
cincy-coder <junocincy@gmail.com>
2025-02-17 10:39:01 +0530

    settings.py

--- Hmart/settings.py
+++ Hmart/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SCREAT_KEY')
+SECRET_KEY = 'django-insecure-u$ik_vwamoqc45p8^-lsxoqlx6xh3tw#=bz^k4g32sj7+i(py0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
